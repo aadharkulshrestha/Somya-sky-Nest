eea7ea7c866cd4ee6123c9d102316cbf5c1c7150
aadharkulshrestha <59021424+aadharkulshrestha@users.noreply.github.com>
2021-01-18 19:54:02 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
-import React from 'react';
-import { createAppContainer, createSwitchNavigator} from 'react-navigation';
+
+//Card,Username in chat , 
+
+
+
+/*import React from 'react';
+import { createAppContainer, createSwitchNavigator,} from 'react-navigation';
 
 
 import WelcomeScreen from './screens/WelcomeScreen';
@@ -19,4 +24,35 @@ const switchNavigator = createSwitchNavigator({
   BottomTab: {screen: AppTabNavigator},
 })
 
-const AppContainer =  createAppContainer(switchNavigator);
+const AppContainer =  createAppContainer(switchNavigator);*/
+
+
+import React from 'react';
+import { createAppContainer, createSwitchNavigator,} from 'react-navigation';
+
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import Exchange from './screens/Exchange';
+import HomeScreen from './screens/HomeScreen';
+import SettingScreen from './screens/SettingScreen';
+import MyBartersScreen from './screens/MyBartersScreen';
+import NotificationScreen from './screens/NotificationsScreen';
+import ReceiverDetailsScreen from './screens/ReceiverDetailsScreen';
+export default function App() {
+  return (
+    <AppContainer/>
+  );
+}
+
+var AppNavigator = createSwitchNavigator({
+  WelcomeScreen:WelcomeScreen,
+  HomeScreen:HomeScreen,
+  Exchange:Exchange,
+  SettingScreen:SettingScreen,
+  MyBartersScreen:MyBartersScreen,
+  NotificationScreen:NotificationScreen,
+  ReceiverDetailsScreen:ReceiverDetailsScreen
+})
+
+const AppContainer =  createAppContainer(AppNavigator);
+
